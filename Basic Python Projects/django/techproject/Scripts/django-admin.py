c59bb1b99fe9dddb5a2a8ea82e3f1104ad7ef6f0
Tyler Johnson <tylerjohnson139@gmail.com>
2019-09-18 18:28:40 -0700

    add hotel django

---
+++ Basic Python Projects/django/techproject/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tyler\desktop\coding\python\basicp~1\django\techpr~1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
